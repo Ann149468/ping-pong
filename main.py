7fe0f63da1618565ad12faf0d9efd72a88b14f1b
Ann149468 <skaral11ldorr@gmail.com>
2026-05-10 19:09:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,11 +33,14 @@ FPS = 60
 run = True
 finish = False
 
+
 font.init()
 my_font = font.SysFont("Arial", 35)
 win = my_font.render("Вы победили!" , True, (255, 255, 0))
 left_r = Player("racket (1).png", 250, 20, 3, (30, 150))
 right_r = Player("racket (1).png", 250, 650, 3, (30, 150))
+ball = GameSprite("tenis_ball.png", 250, 350, 3, (50, 50))
+speed_x, speed_y = 3, 3
 
 while run:
     window.fill((110, 150, 160))
@@ -45,9 +48,16 @@ while run:
         if e.type == QUIT:
             run = False
     if not finish:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        ball.reset()
         left_r.reset()
         right_r.reset()
         left_r.update_l()
         right_r.update_r()
+    if ball.rect.y > 500-50 or ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(left_r, ball) or sprite.collide_rect(right_r, ball):
+        speed_x *= -1
     display.update() 
     clock.tick(60)
